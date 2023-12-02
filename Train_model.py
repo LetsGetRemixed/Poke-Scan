97765a6d357ca9ebd133991b79420e882570d70a
Marel Alvarado <isi6@txstate.edu>
2023-12-02 15:53:00 -0600

    Setup

---
+++ Train_model.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def main():
+    # Create CNN model to classify cards
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def main():
+    # Main program for users to add/remove cards and generate report of their collection
+
+
+
+if __name__ == '__main__':
+    main()
