b4f4f15622ad1d3b0e0b8f8e301779d66472ffce
Marel Alvarado <isi6@txstate.edu>
2023-12-03 00:32:09 -0600

    Implemented get_card_dimension to get the dimension of the pokemon card in the photo

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
 
     labels = list(range(1, 205))
 
-    out = []
+    '''out = []
 
     for i in range(1,205):
         temp = np.array(cv.imread( os.path.join(current_directory,"data_samples",str(str(i) + ".bmp")), cv.IMREAD_GRAYSCALE))
@@ -38,11 +38,54 @@ def main():
     out = np.array(out)
     np.save('train_data.npy', out)
 
-    #train_data = np.load('train_data.npy')
-    #plt.figure()
-    #plt.imshow(train_data[203])
-    #plt.show()
+    train_data = np.load('train_data.npy')
+    plt.figure()
+    plt.imshow(train_data[0])
+    plt.show()'''
 
+    base = np.array(cv.cvtColor(cv.imread( os.path.join(current_directory,"data_samples","testcard5.bmp"), cv.COLOR_RGB2BGR), cv.COLOR_BGR2RGB))
+
+    top_row, bottom_row, width, center = get_card_dimension(base, 9)
+
+    if(bottom_row == len(base)):
+        top_row, bottom_row, width, center = get_card_dimension(base, 2)
+    
+    
+
+
+def get_card_dimension(img, kernel_size):
+    hsv = cv.cvtColor(img, cv.COLOR_RGB2HSV)
+
+    lower = np.array([1,1,1])
+    upper = np.array([255,50,255])
+    mask = cv.inRange(hsv, lower, upper)
+    erodeKernel = np.ones((kernel_size,kernel_size),np.uint8)
+    mask = cv.dilate(mask,erodeKernel)
+
+    nb_components, output, stats, centroids = cv.connectedComponentsWithStats(mask, connectivity=4)
+    max_label, max_size = max([(i, stats[i,cv.CC_STAT_AREA]) for i in range(1, nb_components)], key= lambda x:x[1])
+    mask[output != max_label] = 0
+
+    top_row = int(stats[max_label,cv.CC_STAT_TOP])
+    left_column = int(stats[max_label,cv.CC_STAT_LEFT])
+    bottom_row = top_row + int(stats[max_label,cv.CC_STAT_HEIGHT])
+    right_column = left_column + int(stats[max_label,cv.CC_STAT_WIDTH])
+    width = int((bottom_row - top_row) / 1.4)
+
+    
+    print("top: ",top_row)
+    print("left: ",left_column)
+    print("bottom: ",bottom_row)
+    print("right: ",right_column)
+    print("size: ", (bottom_row - top_row), " x ", width)
+    print("image width: ", len(mask))
+    print("center: ", centroids[0])
+
+    plt.figure()
+    plt.imshow( mask)
+    plt.show()
+
+    return top_row, bottom_row, width, centroids[0]
 
 
 if __name__ == '__main__':
