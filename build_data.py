8684c621852e281708bda3db45bbff7fa802fc50
Marel Alvarado <isi6@txstate.edu>
2023-12-05 15:54:07 -0600

    Included uncompiled training data
    to get training data for main.py run build_data.py

--- build_data.py
+++ build_data.py
@@ -1,12 +1,4 @@
-import os
-import sys
-import cv2 as cv
 import numpy as np
-import tensorflow
-from tensorflow import keras
-from keras.preprocessing.image import ImageDataGenerator
-from keras.preprocessing.image import array_to_img
-from keras.preprocessing.image import img_to_array
 
 from Train_model import train_model
 
@@ -15,11 +7,7 @@ def main():
     # Labels are generated in main.py, where every 200 samples in 'out' are in a class, with 204 unique classes.
     # If modifying amount of data samples, make sure 'labels' in main.py reflects the labels you want
 
-    current_directory = os.path.abspath(os.path.dirname(__file__))
-    parent_directory = os.path.dirname(current_directory)
-    sys.path.append(parent_directory)
-    input_path = os.path.join(current_directory,"data_samples")
-    sample_list = os.listdir(input_path)
+    uncompiled_data = np.load("uncompiled_train_data.npy")
 
 
     #build training data
@@ -35,10 +23,9 @@ def main():
         fill_mode='nearest'
     )
 
-    for i in range(1,int(len(sample_list))):
-        temp = np.array(cv.imread( os.path.join(current_directory,"data_samples",str(str(i) + ".bmp")), cv.IMREAD_GRAYSCALE))
-        temp = cv.resize(temp, (165,230))
-        temp = temp[23:109, 14:152]
+    for i in range(0,int(len(uncompiled_data))):
+        temp = uncompiled_data[i]
+        print("iteration ",i, " of ", int(len(uncompiled_data)))
         out.append(temp)
         for _ in range(199):
             arr = img_to_array(temp)
@@ -49,7 +36,7 @@ def main():
             out.append(augment)
 
     out = np.array(out)
-    print("fin: ",len(out))
+    print("Expecting 40800 data samples, finished with ",len(out), "data samples")
     np.save('train_data.npy', out)
 
 
