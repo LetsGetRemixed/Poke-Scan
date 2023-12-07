312efb5eb35e0614ee02563f40180ccd7af1ec50
Marel Alvarado <isi6@txstate.edu>
2023-12-07 16:13:11 -0600

    Removed unnecessary comments

--- Train_model.py
+++ Train_model.py
@@ -20,23 +20,10 @@ def train_model(cards,labels):
     model.add(Conv2D(filters=64,kernel_size=(3,3),activation='relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
 
-    '''model.add(Conv2D(filters=128,kernel_size=(3,3),activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-
-    model.add(Conv2D(filters=256,kernel_size=(3,3),activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))'''
-
     model.add(Flatten())
     model.add(Dense(256,activation='relu'))
     model.add(Dense(205,activation='softmax'))
     model.add(Dropout(0.2))
-
-    '''model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Flatten())
-    model.add(layers.Dense(256, activation='relu'))
-    model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(204, activation='softmax'))'''
-
 
 
     cards = np.divide(cards,255)
